4da7ee79d421acfeab2d4c0b467bd1e58e55957a
Alberto Riva <alb.bbfge@gmail.com>
2015-07-23 18:43:06 -0400

    changes to top-level commands handling (not finished yet)

--- apple.py
+++ apple.py
@@ -4,7 +4,8 @@
 #                                                   #
 # apple.py - Aracne Processing PipeLine Extensions. #
 #                                                   #
-# (c) 2015, Alberto Riva, Son Le                    #
+# (c) 2015, Alberto Riva (ariva@ufl.edu)            #
+#           Son Le (nuibang@gmail.com               #
 #           University of Florida                   #
 #####################################################
 
@@ -21,8 +22,10 @@ import numpy.random
 ## Check for division by zero in normalization (sigma=0) - Done
 ## Don't output hub genes with no edges - Done
 ## Finish -f and -c arguments
-## Add command for table stats
+## Add command for table stats - Done
 ## Validate arguments for top-level commands
+
+COMMANDS = []
 
 # Utils
 
@@ -49,6 +52,77 @@ account for columns containing gene identifier and name)."""
     with open(filename, "r") as f:
         line = f.readline().split("\t")
         return len(line) - 2
+
+def ensureInt(s):
+    try:
+        return int(s)
+    except ValueError:
+        return False
+
+def ensureFile(s):
+    if os.path.isfile(s):
+        return s
+    else:
+        return False
+
+# Top level commands and usage message
+
+class toplevelCall():
+    name = ""
+    argdesc = ""                # short description of arguments
+    shortdesc = ""              # short description of command
+    longdesc = ""               # long description of command (multi-line)
+
+    def __init__(self, name, argdesc, shortdesc, longdesc="", func=None):
+        self.name = name
+        self.argdesc = argdesc
+        self.shortdesc = shortdesc
+        self.longdesc = longdesc
+
+    def parse(self, args):
+        """Parse the command-line arguments `args' for this command."""
+        pass
+
+    def run(self):
+        """Run this command."""
+        pass
+
+
+
+    # print "Where command-and-arguments can be:\n"
+    # print "  bootstrap filename rounds - bootstrap a file into `rounds' new files.\n"
+    # print "  extract [-a] adj outfile genesfile - extract edges for the genes in file genesfile from the input adj file and write them to outfile in tab-delimited format.\n"
+    # print "  stats filenames... - print statistics on all supplied filenames (in adj format).\n"
+    # print "  random outfile genesfile nsamples - generate random expression data for the genes in `genesfile' on `nsamples' samples using a negative binomial distribution.\n"
+    # print "  consensus [options] outfile infiles... - generate a consensus network from multiple .adj files."
+    # print "    Options:"
+    # print "    [-c countsfile] - write a tab-delimited file with two columns: support, number of occurrences of support."
+    # print "    [-d datafile]   - write a tab-delimited file with five columns: hub, gene, support, sum of MI, P-value."
+    # print "    [-p pval] [-nb] - Use the specified P-value to filter edges in output; -nb disables Bonferroni correction."
+    # print "    [-s support]    - Only output edges found in at least `support' bootstrap files."
+    # print "    [-f fraction]   - Like -s, but determines the support in order to have the specified fraction of edges in output."
+    
+# Bootstrap
+
+class bootstrapCommand(toplevelCall):
+    filename = ""
+    nrounds = 0
+
+    def parse(self, args):
+        if len(args) != 2:
+            return False
+        self.filename = args[0]
+        if not ensureFile(self.filename):
+            print "File {} does not exist.".format(self.filename)
+            return False
+        self.nrounds = ensureInt(args[1])
+        if not self.nrounds:
+            print "Argument `{}' should be a number.".format(args[1])
+            return False
+        return True
+
+    def run(self):
+        bootstrapData(self.filename, self.nrounds)
 
 def samplingWithReplacement(m):
     """Returns a list of m elements in the range 0..m-1 using
@@ -79,55 +153,9 @@ obtained by bootstrapping its columns. Returns the list of new filenames."""
         
     return outfiles
 
-def writeSubmitScript(outfile, datafiles, outdir="results/", pval="1e-7", dpi=0.1):
-    with open(outfile, "w") as o:
-        o.write("#!/bin/bash\n\n")
-        o.write("module load dibig_tools\n\n")
-        o.write("mkdir -p {}\n\n".format(outdir))
-        for df in datafiles:
-            of = changeExtension(df, ".out")
-            oof = changeExtension(df,".adj")
-            o.write("PREV1=`submit aracne-step1.qsub {} {}.aa IDSaa`\n".format(df, of))
-            o.write("PREV2=`submit aracne-step1.qsub {} {}.ab IDSab`\n".format(df, of))
-            o.write("PREV3=`submit aracne-step1.qsub {} {}.ac IDSac`\n".format(df, of))
-            o.write("PREV4=`submit aracne-step1.qsub {} {}.ad IDSad`\n".format(df, of))
-            o.write("submit -after $PREV1 -after $PREV2 -after $PREV3 -after $PREV4 aracne-step2.qsub {} {} {}/{} {} {}\n".format(df, of, outdir, oof, pval, dpi))
-            o.write("\n")
-
-def writeSubmitScriptSplit(outfile, datafiles, split, outdir="results/", pval="1e-7", dpi=0.1):
-    spl = readLines(split)
-    after = ""
-    cnt = 1
-    with open(outfile, "w") as o:
-        o.write("#!/bin/bash\n\n")
-        o.write("module load dibig_tools\n\n")
-        o.write("mkdir -p {}\n\n".format(outdir))
-        for df in datafiles:
-            of = changeExtension(df, ".out")
-            oof = changeExtension(df,".adj")
-            for s in spl:
-                o.write("PREV{}=`submit aracne-step1.qsub {} {}.{} SIDS{}`\n".format(cnt, df, of, s, s))
-                after += " -after $PREV{}".format(cnt)
-                cnt += 1
-
-            o.write("submit {} aracne-step2.qsub {} {} {}/{} {} {}\n".format(after, df, of, outdir, oof, pval, dpi))
-            o.write("\n")
-
-def doAracneBootstrap(filename, outdir="results/", nrounds=100, pval="1e-7", dpi=0.1):
-    files = bootstrapData(filename, nrounds)
-    writeSubmitScript("run.sh", files, outdir=outdir, pval=pval, dpi=dpi)
-    return files
-    
-"""
-STEPS:
-
-1. Call doAracneBootstrap() to write run.sh file;
-2. Execute run.sh to submit jobs to queue;
-3. When jobs are done, delete or compress all .out files (optional);
-4. Call getconsensus.qsub to generate consensus network from all .adj files.
-"""
-
 # Consensus reconstruction
+# This part of the program replaces the getconsensusnet.pl script in the
+# Aracne distribution, which has a number of problems.
 
 class bstable():
     totsupport = None
@@ -150,8 +178,7 @@ class bstable():
         self.supportDist = {}
 
     def internName(self, name):
-        """Return the index associated with `name', creating
-a new one if necessary."""
+        """Return the index associated with `name', creating a new one if necessary."""
         if name in self.names:
             return self.names[name]
         else:
@@ -162,13 +189,11 @@ a new one if necessary."""
             return new
 
     def decodeName(self, nameid):
-        """Return the name associated with index `nameid' (this
-is the inverse of internName)."""
+        """Return the name associated with index `nameid' (this is the inverse of internName)."""
         return self.invnames[nameid]
 
     def addEdge(self, hub, gene, mi):
-        """Add an edge from `hub' to `gene', with
-mutual information `mi'."""
+        """Add an edge from `hub' to `gene', with mutual information `mi'."""
         hubid = self.internName(hub)
         geneid = self.internName(gene)
         if hubid in self.totsupport:
@@ -328,12 +353,14 @@ times it occurs in totsupport."""
 # Extract a subset of genes from an .adj file
 
 def addToSeen(seen, g1, g2):
+    """Record the fact that an edge connecting `g1' and `g2' was seen in the current network."""
     if g1 in seen:
         seen[g1].append(g2)
     else:
         seen[g1] = [g2]
 
 def wasNotSeen(seen, g1, g2):
+    """Returns True if an edge connecting `g1' to `g2' has NOT been seen yet in the current network."""
     if g1 in seen:
         return not (g2 in seen[g1])
     else:
@@ -344,6 +371,7 @@ def extractGeneSubset(infile, outfile, genes, both=False):
 from adj file `infile' and write them to `outfile' in tab-delimited format (g1, g2, mi).
 Also filters out symmetrical edges (ie, each pair is written only once regardless of order).
 If `both' is True, only output edges where both genes belong to the list."""
+
     print "Extracting {} genes from {} (both={})".format(len(genes), infile, both)
     seen = {}
     with open(outfile, "w") as out:
@@ -369,31 +397,7 @@ If `both' is True, only output edges where both genes belong to the list."""
                                         out.write("{}\t{}\t{}\n".format(hub, gene, parsed[i+1]))
                                         addToSeen(seen, hub, gene)
 
-# Some random utils
-
-def writeReconstruct(outfile, start, end):
-    with open(outfile, "w") as out:
-        out.write("#!/bin/bash\n\n")
-        for i in range(start, end):
-            out.write("echo -n {} \n".format(i))
-            out.write("submit ../reconstruct.qsub ../SPLIT all.gene_RPKM.aracne.nozero-{}.adj {}/all.gene_RPKM.aracne.nozero-{}\n".format(i, i, i))
-
-def writeStep2(outfile, start, end, tfs=False):
-    with open(outfile, "w") as out:
-        out.write("#!/bin/bash\n\n")
-        for i in range(start, end):
-            if tfs:
-                out.write("submit aracne.qsub ../all.gene_RPKM.aracne.nozero.txt all.gene_RPKM.aracne.nozero-{}.dpi.adj dpi=0.1 adj=all.gene_RPKM.aracne.nozero-{}.adj tfs={}\n".format(i, i, tfs))
-            else:
-                out.write("submit aracne.qsub ../all.gene_RPKM.aracne.nozero.txt all.gene_RPKM.aracne.nozero-{}.dpi.adj dpi=0.1 adj=all.gene_RPKM.aracne.nozero-{}.adj\n".format(i, i))
-
-def writeStep2b(outfile, start, end):
-    with open(outfile, "w") as out:
-        out.write("#!/bin/bash\n\n")
-        for i in range(start, end):
-            dpi = "results/all.gene_RPKM.aracne.nozero-{}.dpi.adj".format(i)
-            if not os.path.isfile(dpi):
-                out.write("submit aracne.qsub ../all.gene_RPKM.aracne.nozero.txt all.gene_RPKM.aracne.nozero-{}.dpi.adj dpi=0.1 adj=all.gene_RPKM.aracne.nozero-{}.adj\n".format(i, i))
+# Stats command
 
 def aracneTableStats(filename):
     """Returns a tuple containing: the number of rows in the file, the sum of the
@@ -429,21 +433,6 @@ def aracneAllStats(outfile):
 
 # Main function and top-level commands
 
-def usage():
-    print "Usage: apple.py command-and-arguments...\n"
-    print "Where command-and-arguments can be:\n"
-    print "  bootstrap filename rounds - bootstrap a file into `rounds' new files.\n"
-    print "  extract [-a] adj outfile genesfile - extract edges for the genes in file genesfile from the input adj file and write them to outfile in tab-delimited format.\n"
-    print "  stats filenames... - print statistics on all supplied filenames (in adj format).\n"
-    print "  random outfile genesfile nsamples - generate random expression data for the genes in `genesfile' on `nsamples' samples using a negative binomial distribution.\n"
-    print "  consensus [options] outfile infiles... - generate a consensus network from multiple .adj files."
-    print "    Options:"
-    print "    [-c countsfile] - write a tab-delimited file with two columns: support, number of occurrences of support."
-    print "    [-d datafile]   - write a tab-delimited file with five columns: hub, gene, support, sum of MI, P-value."
-    print "    [-p pval] [-nb] - Use the specified P-value to filter edges in output; -nb disables Bonferroni correction."
-    print "    [-s support]    - Only output edges found in at least `support' bootstrap files."
-    print "    [-f fraction]   - Like -s, but determines the support in order to have the specified fraction of edges in output."
-    
 class consensusArgs():
     outfile = None
     csvfile = None
@@ -572,29 +561,152 @@ def main():
     if len(sys.argv) == 1:
         return usage()
 
-    command = sys.argv[1]
-    if command == "consensus":
-        parsed = consensusArgs(sys.argv[2:])
-        runConsensus(parsed.outfile, parsed.infiles, parsed)
-    elif command == "bootstrap":
-        bootstrapData(sys.argv[2], int(sys.argv[3]))
-    elif command == "extract":
-        parsed = extractArgs(sys.argv[2:])
-        extractGeneSubset(parsed.adjfile, parsed.outfile, parsed.geneslist, both=parsed.both)
-    elif command == "stats":
-        doAracneStats(sys.argv[2:])
-    elif command == "translate":
-        tablefile = sys.argv[2]
-        infile = sys.argv[3]
-        outfile = sys.argv[4]
-        translateFile(tablefile, infile, outfile)
-    elif command == "random":
-        doRandomDataset(sys.argv[2], sys.argv[3], int(sys.argv[4]))
-    else:
-        usage()
+    name = sys.argv[1]
+    command = findCommand(name)
 
+    if command == None:
+        return usage()
+
+    if command.parse(sys.argv[2:]):
+        command.run()
+    else:
+        return usage(name)
+
+
+    # if len(sys.argv) == 2:
+    #     return usage(command)
+
+    # elif command == "consensus":
+    #     parsed = consensusArgs(sys.argv[2:])
+    #     runConsensus(parsed.outfile, parsed.infiles, parsed)
+    # elif command == "bootstrap":
+    #     bootstrapData(sys.argv[2], int(sys.argv[3]))
+    # elif command == "extract":
+    #     parsed = extractArgs(sys.argv[2:])
+    #     extractGeneSubset(parsed.adjfile, parsed.outfile, parsed.geneslist, both=parsed.both)
+    # elif command == "stats":
+    #     doAracneStats(sys.argv[2:])
+    # elif command == "translate":
+    #     tablefile = sys.argv[2]
+    #     infile = sys.argv[3]
+    #     outfile = sys.argv[4]
+    #     translateFile(tablefile, infile, outfile)
+    # elif command == "random":
+    #     doRandomDataset(sys.argv[2], sys.argv[3], int(sys.argv[4]))
+    # else:
+    #     usage()
+
+# Everything from this point on is not part of the apple.py program. These functions
+# are leftovers from tests and one-off scripts.
+
+def writeSubmitScript(outfile, datafiles, outdir="results/", pval="1e-7", dpi=0.1):
+    with open(outfile, "w") as o:
+        o.write("#!/bin/bash\n\n")
+        o.write("module load dibig_tools\n\n")
+        o.write("mkdir -p {}\n\n".format(outdir))
+        for df in datafiles:
+            of = changeExtension(df, ".out")
+            oof = changeExtension(df,".adj")
+            o.write("PREV1=`submit aracne-step1.qsub {} {}.aa IDSaa`\n".format(df, of))
+            o.write("PREV2=`submit aracne-step1.qsub {} {}.ab IDSab`\n".format(df, of))
+            o.write("PREV3=`submit aracne-step1.qsub {} {}.ac IDSac`\n".format(df, of))
+            o.write("PREV4=`submit aracne-step1.qsub {} {}.ad IDSad`\n".format(df, of))
+            o.write("submit -after $PREV1 -after $PREV2 -after $PREV3 -after $PREV4 aracne-step2.qsub {} {} {}/{} {} {}\n".format(df, of, outdir, oof, pval, dpi))
+            o.write("\n")
+
+def writeSubmitScriptSplit(outfile, datafiles, split, outdir="results/", pval="1e-7", dpi=0.1):
+    spl = readLines(split)
+    after = ""
+    cnt = 1
+    with open(outfile, "w") as o:
+        o.write("#!/bin/bash\n\n")
+        o.write("module load dibig_tools\n\n")
+        o.write("mkdir -p {}\n\n".format(outdir))
+        for df in datafiles:
+            of = changeExtension(df, ".out")
+            oof = changeExtension(df,".adj")
+            for s in spl:
+                o.write("PREV{}=`submit aracne-step1.qsub {} {}.{} SIDS{}`\n".format(cnt, df, of, s, s))
+                after += " -after $PREV{}".format(cnt)
+                cnt += 1
+
+            o.write("submit {} aracne-step2.qsub {} {} {}/{} {} {}\n".format(after, df, of, outdir, oof, pval, dpi))
+            o.write("\n")
+
+"""
+STEPS:
+
+1. Call doAracneBootstrap() to write run.sh file;
+2. Execute run.sh to submit jobs to queue;
+3. When jobs are done, delete or compress all .out files (optional);
+4. Call getconsensus.qsub to generate consensus network from all .adj files.
+"""
+
+def doAracneBootstrap(filename, outdir="results/", nrounds=100, pval="1e-7", dpi=0.1):
+    files = bootstrapData(filename, nrounds)
+    writeSubmitScript("run.sh", files, outdir=outdir, pval=pval, dpi=dpi)
+    return files
+    
+# Some random utils
+
+def writeReconstruct(outfile, start, end):
+    with open(outfile, "w") as out:
+        out.write("#!/bin/bash\n\n")
+        for i in range(start, end):
+            out.write("echo -n {} \n".format(i))
+            out.write("submit ../reconstruct.qsub ../SPLIT all.gene_RPKM.aracne.nozero-{}.adj {}/all.gene_RPKM.aracne.nozero-{}\n".format(i, i, i))
+
+def writeStep2(outfile, start, end, tfs=False):
+    with open(outfile, "w") as out:
+        out.write("#!/bin/bash\n\n")
+        for i in range(start, end):
+            if tfs:
+                out.write("submit aracne.qsub ../all.gene_RPKM.aracne.nozero.txt all.gene_RPKM.aracne.nozero-{}.dpi.adj dpi=0.1 adj=all.gene_RPKM.aracne.nozero-{}.adj tfs={}\n".format(i, i, tfs))
+            else:
+                out.write("submit aracne.qsub ../all.gene_RPKM.aracne.nozero.txt all.gene_RPKM.aracne.nozero-{}.dpi.adj dpi=0.1 adj=all.gene_RPKM.aracne.nozero-{}.adj\n".format(i, i))
+
+def writeStep2b(outfile, start, end):
+    with open(outfile, "w") as out:
+        out.write("#!/bin/bash\n\n")
+        for i in range(start, end):
+            dpi = "results/all.gene_RPKM.aracne.nozero-{}.dpi.adj".format(i)
+            if not os.path.isfile(dpi):
+                out.write("submit aracne.qsub ../all.gene_RPKM.aracne.nozero.txt all.gene_RPKM.aracne.nozero-{}.dpi.adj dpi=0.1 adj=all.gene_RPKM.aracne.nozero-{}.adj\n".format(i, i))
+
+COMMANDS = [bootstrapCommand("bootstrap", "filename rounds", "bootstrap a file into `rounds' new files."),
+            toplevelCall("consensus", "[options] outfile infiles...", "generate a consensus network from multiple .adj files."),
+            toplevelCall("extract", "[-a] adj outfile genesfile",
+                         "extract edges for the genes in file genesfile from the input adj file and write them to outfile in tab-delimited format."),
+            toplevelCall("random", "outfile genesfile nsamples",
+                         "generate random expression data for the genes in `genesfile' on `nsamples' samples using a negative binomial distribution."),
+            toplevelCall("stats", "filenames...", "print statistics on all supplied filenames (in adj format)."),
+            toplevelCall("translate", "table infile outfile", "translate identifiers in `infile' writing them to `outfile'.")]
+
+def findCommand(name):
+    global COMMANDS
+    for c in COMMANDS:
+        if c.name == name:
+            return c
+    return None
+
+def usage(what=None):
+    global COMMANDS
+
+    if what == None:
+        print "Usage: apple.py command arguments...\n"
+        print "The following commands are available:\n"
+        for c in COMMANDS:
+            print "  {} - {}".format(c.name, c.shortdesc)
+        print "\nUse 'apple.py command' to get a description of each command.\n"
+
+    else:
+        c = findCommand(what)
+        if c == None:
+            usage()
+        else:
+            print "Usage: apple.py {} {} - {}".format(c.name, c.argdesc, c.shortdesc)
+            
 if __name__ == "__main__":
     print "apple.py - Aracne Processing PipeLine Extensions.\n"
     main()
     
-
